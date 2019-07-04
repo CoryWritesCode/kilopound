67ea21570a11ab9d4e4d8fb240e8259f3e3119b4
CoryWritesCode <corycunningham2010@gmail.com>
2019-07-03 23:30:11 -0500

    home screen done

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -14,9 +14,6 @@ import { WEIGHTS } from '../globals/system';
 import NativeTachyons from 'react-native-style-tachyons';
 
 const styles = StyleSheet.create({
-  home: {
-    backgroundColor: COLORS.BGCOLOR,
-  },
   weightContainer: {
     height: '75%',
   }
@@ -31,19 +28,19 @@ function HomeScreen() {
 
 
   return (
-    <SafeAreaView cls='flx-i' style={styles.home}>
-      <View cls='flx-i pt2' style={styles.home}>
+    <SafeAreaView cls='flx-i bg-bgcolor'>
+      <View cls='flx-i pt2 bg-bgcolor'>
         <StatusBar barStyle="light-content" />
         <View cls='flx-row flx-wrap jcc asc w5' style={styles.weightContainer}>
           {weights.map((weight) => <AddWeight key={weight} amount={weight} handlePress={() => setTotal(total + weight)} />)}
         </View>
         <View cls='flx-wrap flx-row jcsa aic'>
           <View>
-            <Text cls='ph3 pv2 mv1' style={{ color: COLORS.FONT_COLOR }}>Total:</Text>
-            <Text cls='ph3' style={{ color: COLORS.FONT_COLOR }}>{total} {metric}</Text>
+            <Text cls='ph3 pv2 mv1 font'>Total:</Text>
+            <Text cls='ph3 font'>{total} {metric}</Text>
           </View>
           <View cls='jcc aic'>
-            <Text cls='ph3 pv2 mv1' style={{ color: COLORS.FONT_COLOR }}>Change Metric</Text>
+            <Text cls='ph3 pv2 mv1 font'>Change Metric</Text>
             <Switch
               trackColor={{ false: COLORS.PRIMARY, true: COLORS.PRIMARY }}
               onValueChange={() => setIsBritish(!isBritish)}
@@ -53,8 +50,8 @@ function HomeScreen() {
           </View>
         </View>
         <View style={{ marginVertical: 10 }}>
-          <TouchableOpacity cls='ba br2 ph3 pv2 mh3 mv1 asc' style={{ borderColor: COLORS.PRIMARY }} onPress={() => setTotal(0)}>
-            <Text style={{ color: COLORS.PRIMARY }}>Reset</Text>
+          <TouchableOpacity cls='ba br2 ph3 pv2 mh3 mv1 asc b--primary' onPress={() => setTotal(0)}>
+            <Text cls='primary'>Reset</Text>
           </TouchableOpacity>
         </View>
       </View>
